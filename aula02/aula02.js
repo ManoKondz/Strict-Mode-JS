3c111426af573f74db1f6600dda351aee733662f
Enzo Gabriel <122542882+ManoKondz@users.noreply.github.com>
2023-07-16 13:19:06 -0300

    Add files via upload

---
+++ aula02/aula02.js
@@ -0,0 +1,7 @@
+"use strict" //Modo Estrito
+
+let nome="Enzo em variável";
+let strvar="Enzo em variável e string";
+console.log("Enzo Gabriel em string");
+console.log(nome);
+console.log("Aluno:" + strvar);
